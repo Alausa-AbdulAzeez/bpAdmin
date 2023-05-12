c0039d6b1ea04faea61123ff142e632a767deed1
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-05-12 15:09:55 +0100

    added schedule candidate link to manage candidate page

--- src/pages/mangeCandidates/ManageCandidates.jsx
+++ src/pages/mangeCandidates/ManageCandidates.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './manageCandidates.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Topber from '../../components/topbar/Topber'
+import { TextField } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { RiAddLine } from 'react-icons/ri'
 
 const ManageCandidates = () => {
   return (
@@ -10,8 +13,28 @@ const ManageCandidates = () => {
       <div className='manageCandidatesRight'>
         <Topber />
         <div className='manageCandidatesMainWrapper'>
-          <div className='manageTestsMainTop'>
+          <div className='manageCandidatesMainTop'>
             <h3>All Tests</h3>
+            <div className='manageCandidatesMainTopForm'>
+              <div className='formAndSearchWrapperMc'>
+                <TextField
+                  id='outlined-search'
+                  label='Candidate name'
+                  type='search'
+                  className='candidateName'
+                />
+
+                <div className='manageCandidatesBtn'>Search</div>
+              </div>
+              <Link to='/scheduleCandidate'>
+                <button className='scheduleCandidateBtn'>
+                  Schedule Candidate
+                  <span>
+                    <RiAddLine className='addIcon' />
+                  </span>
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
