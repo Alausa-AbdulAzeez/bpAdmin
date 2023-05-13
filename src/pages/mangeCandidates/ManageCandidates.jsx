ac27cae06a01520427ad5b0d190f8041eb9c2270
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-05-13 07:52:16 +0100

    added error page to manageCandidates page

--- src/pages/mangeCandidates/ManageCandidates.jsx
+++ src/pages/mangeCandidates/ManageCandidates.jsx
@@ -1,81 +1,87 @@
-import React, { useEffect, useState } from 'react'
-import './manageCandidates.scss'
-import Sidebar from '../../components/sidebar/Sidebar'
-import Topber from '../../components/topbar/Topber'
-import { Box, TextField } from '@mui/material'
-import { Link } from 'react-router-dom'
-import { RiAddLine } from 'react-icons/ri'
-import { DataGrid } from '@mui/x-data-grid'
-import { MdEdit } from 'react-icons/md'
-import { BsTrashFill } from 'react-icons/bs'
-import { publicRequest } from '../../functions/requestMethods'
+import React, { useEffect, useState } from "react";
+import "./manageCandidates.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Topber from "../../components/topbar/Topber";
+import { Autocomplete, Box, TextField } from "@mui/material";
+import { Link } from "react-router-dom";
+import { RiAddLine } from "react-icons/ri";
+import { DataGrid } from "@mui/x-data-grid";
+import { MdEdit } from "react-icons/md";
+import { BsTrashFill } from "react-icons/bs";
+import { publicRequest } from "../../functions/requestMethods";
+import Loading from "../../components/loading/Loading";
+import Error from "../../components/error/Error";
 
 const ManageCandidates = () => {
   // PAGE SIZE OF TABLE
-  const [pageSize, setPageSize] = useState(5)
+  const [pageSize, setPageSize] = useState(5);
+
+  // CLIENTS DATA
+  const [clients, setClients] = useState([]);
+  const [clientId, setClientId] = useState(null);
 
   // TABLE COLUMN DATA
   const columns = [
     {
-      field: 'candidateName',
-      headerName: 'Name',
+      field: "candidateName",
+      headerName: "Name",
       width: 200,
       editable: false,
     },
     {
-      field: 'phoneNumber',
-      headerName: 'Phone Number',
+      field: "phoneNumber",
+      headerName: "Phone Number",
       width: 120,
       editable: false,
     },
     {
-      field: 'appointmentdate',
-      headerName: 'Appointment Date',
+      field: "appointmentdate",
+      headerName: "Appointment Date",
       width: 100,
       editable: false,
-      description: 'The candidate shoul be present by this date',
+      description: "The candidate shoul be present by this date",
     },
     {
-      field: 'testcategory',
-      headerName: 'Test Category',
+      field: "testcategory",
+      headerName: "Test Category",
       width: 100,
       editable: false,
     },
     {
-      field: 'status',
-      headerName: 'Status',
+      field: "status",
+      headerName: "Status",
       width: 100,
       editable: false,
     },
 
     {
-      field: 'createdDate',
-      headerName: 'Date Created',
+      field: "createdDate",
+      headerName: "Date Created",
       width: 100,
       editable: false,
     },
     {
-      field: 'fullName',
-      headerName: 'Action',
+      field: "fullName",
+      headerName: "Action",
 
       sortable: false,
       width: 260,
       renderCell: (props) => {
         return (
-          <div className='buttons'>
-            <div className='editWrapper'>
-              <div className='edit'>Edit</div>
-              <MdEdit className='editIcon' />
+          <div className="buttons">
+            <div className="editWrapper">
+              <div className="edit">Edit</div>
+              <MdEdit className="editIcon" />
             </div>
-            <div className='deleteWrapper'>
-              <div className='delete'>Delete</div>
-              <BsTrashFill className='deleteIcon' />
+            <div className="deleteWrapper">
+              <div className="delete">Delete</div>
+              <BsTrashFill className="deleteIcon" />
             </div>
           </div>
-        )
+        );
       },
     },
-  ]
+  ];
 
   // TABLE ROW DATA
   const [rows, setRows] = useState([
@@ -88,86 +94,142 @@ const ManageCandidates = () => {
     //   createdDate: '1 Jann 2023',
     //   id: 1,
     // },
-  ])
-  // LOADING DATA
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
+  ]);
+  // LOADING AND ERROR DATA
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   // FUNCTION TO GET AND SET ALL CANDIDATES
   const getAllCandidates = async () => {
     try {
-      setLoading(true)
-      const res = await publicRequest.get('/Candidate')
+      setLoading(true);
+      const res = await publicRequest.get("/Candidate");
 
       if (res.data) {
-        console.log(res.data)
-        setRows(res.data?.data)
-        setLoading(false)
+        console.log(res.data);
+        setRows(res.data?.data?.reverse());
+        setLoading(false);
       } else {
-        console.log(res.data)
+        console.log(res.data);
       }
     } catch (error) {
-      setLoading(false)
-      setError(true)
-      console.log(error)
+      setLoading(false);
+      setError(true);
+      setErrorMessage(error);
+
+      console.log(error);
     }
-  }
+  };
   // END OF FUNCTION TO GET AND SET ALL CANDIDATES
+  //  FUNCTIONALITIES FOR FETCHING AND SETTING CLIENTS
+
+  const getAllClients = async () => {
+    try {
+      const res = await publicRequest.get("Client/Client-list");
+
+      if (res.data) {
+        setClients(res.data.data);
+        console.log(res.data);
+      } else {
+        console.log(res.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //  END OF FUNCTIONALITIES FOR FETCHING AND SETTING CLIENTS
+
+  // FUNCTION FOR SETTING CLIENT ID
+  const handlescheduleCandidateInfo = (e, dataName, data) => {
+    setClientId(data?.clientId);
+  };
+  //END OF FUNCTION FOR SETTING CLIENT ID
 
   // USE EFFECT TO GET ALL CANDIDATES AS THE PAGE LOADS
   useEffect(() => {
-    getAllCandidates()
-  }, [])
+    getAllCandidates();
+  }, []);
+
+  // use effect to call the getAllClients function as the page loads
+  useEffect(() => {
+    getAllClients();
+  }, []);
+  // end of use effect to call the getAllClients function as the page loads
 
   return (
-    <div className='manageCandidatesWrapper'>
+    <div className="manageCandidatesWrapper">
       <Sidebar />
-      <div className='manageCandidatesRight'>
+      <div className="manageCandidatesRight">
         <Topber />
-        <div className='manageCandidatesMainWrapper'>
-          <div className='manageCandidatesMainTop'>
-            <h3>All Tests</h3>
-            <div className='manageCandidatesMainTopForm'>
-              <div className='formAndSearchWrapperMc'>
-                <TextField
-                  id='outlined-search'
-                  label='Candidate name'
-                  type='search'
-                  className='candidateName'
-                />
+        {loading || error ? (
+          loading ? (
+            <Loading />
+          ) : (
+            <Error errorMessage={errorMessage && errorMessage} />
+          )
+        ) : (
+          <div className="manageCandidatesMainWrapper">
+            <div className="manageCandidatesMainTop">
+              <h3>All Tests</h3>
+              <div className="manageCandidatesMainTopForm">
+                <div className="formAndSearchWrapperMc">
+                  <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={clients}
+                    getOptionLabel={(option) =>
+                      `${option.clientName} ${option.email}`
+                    }
+                    onChange={(e, option) =>
+                      handlescheduleCandidateInfo(e, "clientid", option)
+                    }
+                    sx={{ width: 300 }}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Client Name" />
+                    )}
+                  />
+                  <TextField
+                    id="outlined-search"
+                    label="Candidate name"
+                    type="search"
+                    className="candidateName"
+                  />
 
-                <div className='manageCandidatesBtn'>Search</div>
-              </div>
-              <Link to='/scheduleCandidate'>
-                <button className='scheduleCandidateBtn'>
+                  <div className="manageCandidatesBtn">Search</div>
+                </div>
+                {/* <Link to="/scheduleCandidate">
+                <button className="scheduleCandidateBtn">
                   Schedule Candidate
                   <span>
-                    <RiAddLine className='addIcon' />
+                    <RiAddLine className="addIcon" />
                   </span>
                 </button>
-              </Link>
+              </Link> */}
+              </div>
+            </div>
+            <div className="partnerLabsMainBottom">
+              <Box sx={{ height: 400, width: "100%" }}>
+                <DataGrid
+                  rows={rows}
+                  columns={columns}
+                  pageSize={pageSize}
+                  checkboxSelection
+                  disableSelectionOnClick
+                  experimentalFeatures={{ newEditingApi: true }}
+                  onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                  rowsPerPageOptions={[5, 10, 20]}
+                  pagination
+                  getRowId={(row) => row?.canditateId}
+                />
+              </Box>
             </div>
           </div>
-          <div className='partnerLabsMainBottom'>
-            <Box sx={{ height: 400, width: '100%' }}>
-              <DataGrid
-                rows={rows}
-                columns={columns}
-                pageSize={pageSize}
-                checkboxSelection
-                disableSelectionOnClick
-                experimentalFeatures={{ newEditingApi: true }}
-                onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-                rowsPerPageOptions={[5, 10, 20]}
-                pagination
-                getRowId={(row) => row?.canditateId}
-              />
-            </Box>
-          </div>
-        </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ManageCandidates
+export default ManageCandidates;
