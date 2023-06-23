b881d55d220c1c18b50e5c6673c78b11e0cf10ae
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-06-23 13:20:55 +0100

    completed profile page

--- src/pages/addTest/AddTest.jsx
+++ src/pages/addTest/AddTest.jsx
@@ -1,165 +1,164 @@
-import React, { useEffect, useState } from "react";
-import Sidebar from "../../components/sidebar/Sidebar";
-import Topber from "../../components/topbar/Topber";
-import "./addTest.scss";
-import AlertDialogSlide from "../../components/Dialogue";
-import { Autocomplete, TextField } from "@mui/material";
-import { privateRequest, publicRequest } from "../../functions/requestMethods";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useEffect, useState } from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Topber from '../../components/topbar/Topber'
+import './addTest.scss'
+import AlertDialogSlide from '../../components/Dialogue'
+import { privateRequest, publicRequest } from '../../functions/requestMethods'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const AddTest = () => {
-  const [open, setOpen] = React.useState(false);
-  const toastId = React.useRef(null);
+  const [open, setOpen] = React.useState(false)
+  const toastId = React.useRef(null)
 
   const handleClickOpen = () => {
-    setOpen(true);
-  };
+    setOpen(true)
+  }
 
   const handleClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   // FUNCTIONALITIES PARTAINING TO FETCHING AND SETTING ROLES
 
-  const [roles, setRoles] = useState([]);
+  const [roles, setRoles] = useState([])
 
   // fetch roles
   const getRoles = async () => {
     try {
-      const res = await publicRequest.get("/Account/roles");
+      const res = await publicRequest.get('/Account/roles')
 
       if (res) {
-        console.log(res.data.data);
-        setRoles(res.data.data);
+        console.log(res.data.data)
+        setRoles(res.data.data)
       } else {
-        console.log(res);
+        console.log(res)
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
   // end of fetch roles
 
   // use effect for fetching roles
   useEffect(() => {
-    getRoles();
-  }, []);
+    getRoles()
+  }, [])
   // end of use effect for fetching roles
 
   // END FUNCTIONALITIES PARTAINING TO ROLES
 
   // FUNCTIONALITIES FOR CREATING A NEW TEST
   const [test, setTest] = useState({
-    testId: "",
-    description: "",
-    testName: "",
-  });
+    testId: '',
+    description: '',
+    testName: '',
+  })
 
   // function for setting test info
   const handleTestData = (e, dataName, data) => {
     setTest((prev) => {
-      return { ...prev, [dataName]: data ? data.name : e.target.value };
-    });
-  };
+      return { ...prev, [dataName]: data ? data.name : e.target.value }
+    })
+  }
   // end of function for setting test info
 
   const createTest = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     // const id = toast.loading('Please wait...')
-    toastId.current = toast("Please wait...", {
+    toastId.current = toast('Please wait...', {
       autoClose: false,
       isLoading: true,
-    });
-    console.log(test);
+    })
+    console.log(test)
     try {
-      await privateRequest.post("/Test", test).then((response) => {
+      await privateRequest.post('/Test', test).then((response) => {
         toast.update(toastId.current, {
-          render: "Test has been created succesfully!",
-          type: "success",
+          render: 'Test has been created succesfully!',
+          type: 'success',
           isLoading: false,
           autoClose: 3000,
-        });
-      });
+        })
+      })
     } catch (error) {
-      console.log(error.response);
+      console.log(error.response)
       toast.update(toastId.current, {
-        type: "error",
+        type: 'error',
         autoClose: 3000,
         isLoading: false,
         render: `${
           error.response.data.title ||
           error.response.data.description ||
-          "Something went wrong, please try again"
+          'Something went wrong, please try again'
         }`,
-      });
+      })
     }
-  };
+  }
 
   //END OF FUNCTIONALITIES FOR CREATING A NEW TEST
 
   return (
     <>
       <ToastContainer />
-      <div className="addLabWrapper">
+      <div className='addTestWrapper'>
         <AlertDialogSlide
           open={open}
           handleClose={handleClose}
-          title="Cancel"
-          link="/manageTests"
-          message="Warning!! Your changes have not been saved. Are you sure you want to leave this page? Any unsaved changes will be lost."
+          title='Cancel'
+          link='/manageTests'
+          message='Warning!! Your changes have not been saved. Are you sure you want to leave this page? Any unsaved changes will be lost.'
         />
         <Sidebar />
-        <div className="addLabRight">
+        <div className='addTestRight'>
           <Topber />
-          <div className="addLabMainWrapper">
+          <div className='addTestMainWrapper'>
             <h2> Add New Test</h2>
-            <form className="addLabFormWrapper" onSubmit={(e) => createTest(e)}>
-              <div className="inputsWrapper">
-                <div className="singleInput">
+            <form
+              className='addTestFormWrapper'
+              onSubmit={(e) => createTest(e)}
+            >
+              <div className='addTestInputsWrapper'>
+                <div className='addTestSingleInput'>
                   <p>Test Name</p>
-                  <div className="inputWrapper">
+                  <div className='addTestInputWrapper'>
                     <input
-                      type="text"
-                      className="input"
-                      onChange={(e) => handleTestData(e, "testName")}
+                      type='text'
+                      className='addTestInput'
+                      onChange={(e) => handleTestData(e, 'testName')}
                       required
                     />
                   </div>
                 </div>
 
-                <div className="singleInput">
+                <div className='addTestSingleInput'>
                   <p>Description</p>
-                  <div className="textAreaWrapper">
+                  <div className='textAreaWrapper'>
                     <textarea
-                      type="text"
-                      className="textArea"
-                      cols={50}
-                      rows={10}
-                      onChange={(e) => handleTestData(e, "description")}
-                      style={{ padding: "10px", outline: "none" }}
+                      type='text'
+                      className='textArea'
+                      cols={5}
+                      rows={7}
+                      onChange={(e) => handleTestData(e, 'description')}
+                      style={{ padding: '10px', outline: 'none' }}
                       required
                     />
                   </div>
                 </div>
               </div>
-              <div className="bottomButtons">
-                <button
-                  className="cancelClientEditBtn"
-                  onClick={handleClickOpen}
-                >
-                  Cancel
-                </button>
-                <button className="addLabEditBtn" type="submit">
-                  Save
-                </button>
-              </div>
+              {/* <div className='bottomButtons'> */}
+              <button className='cancelTestBtn' onClick={handleClickOpen}>
+                Cancel
+              </button>
+              <button className='addTestEditBtn' type='submit'>
+                Save
+              </button>
+              {/* </div> */}
             </form>
           </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default AddTest;
+export default AddTest
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Topber from '../../components/topbar/Topber'
 import './profile.scss'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  // GET CURRENT LOGGED IN USER
+  const { currentUser } = useSelector((state) => state?.user)
+  const loggedInUserRole = currentUser?.data?.role
+
+  const { fullName, phoneNumber, email } = currentUser?.data?.profile
+
   return (
     <div className='profileWrapper'>
       <Sidebar />
@@ -14,40 +21,52 @@ const Profile = () => {
           <div className='formWrapper'>
             <div className='inputsWrapper'>
               <div className='singleInput'>
-                <p>First Name</p>
+                <p>Fullname</p>
                 <div className='inputWrapper'>
-                  <input type='text' className='input' />
+                  <input
+                    type='text'
+                    className='input'
+                    disabled
+                    value={fullName}
+                  />
                 </div>
               </div>
-              <div className='singleInput'>
-                <p>Last Name</p>
-                <div className='inputWrapper'>
-                  <input type='text' className='input' />
-                </div>
-              </div>
+
               <div className='singleInput'>
                 <p>Email</p>
                 <div className='inputWrapper'>
-                  <input type='email' className='input' />
+                  <input
+                    type='email'
+                    className='input'
+                    value={email}
+                    disabled
+                  />
                 </div>
               </div>
               <div className='singleInput'>
                 <p>Phone Number</p>
                 <div className='inputWrapper'>
-                  <input type='number' className='input' />
+                  <input
+                    type='number'
+                    className='input'
+                    disabled
+                    value={phoneNumber}
+                  />
                 </div>
               </div>
               <div className='singleInput'>
                 <p>Role</p>
                 <div className='inputWrapper'>
-                  <select name='roleSelect' id='roleSelect' className='input'>
-                    <option value='Admin'>Admin</option>
-                    <option value='labScientist'>Lab Scientist</option>
-                  </select>
+                  <input
+                    type='text'
+                    className='input'
+                    disabled
+                    value={loggedInUserRole}
+                  />
                 </div>
               </div>
             </div>
-            <button className='profileEditBtn'>Edit</button>
+            {/* <button className='profileEditBtn'>Edit</button> */}
           </div>
         </div>
       </div>
