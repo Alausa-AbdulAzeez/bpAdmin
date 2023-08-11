228549b57308c861ae3c8e257035e217790ee210
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-08-11 21:08:57 +0100

    made profile page responsive

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,77 +1,84 @@
-import React from 'react'
-import Sidebar from '../../components/sidebar/Sidebar'
-import Topber from '../../components/topbar/Topber'
-import './profile.scss'
-import { useSelector } from 'react-redux'
+import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Topber from "../../components/topbar/Topber";
+import "./profile.scss";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
   // GET CURRENT LOGGED IN USER
-  const { currentUser } = useSelector((state) => state?.user)
-  const loggedInUserRole = currentUser?.data?.role
-
-  const { fullName, phoneNumber, email } = currentUser?.data?.profile
+  const { currentUser } = useSelector((state) => state?.user);
+  const loggedInUserRole = currentUser?.data?.role;
+  const { fullName, phoneNumber, email } = currentUser?.data?.profile;
+  const userData = currentUser?.data;
 
   return (
-    <div className='profileWrapper'>
+    <div className="profileWrapper">
       <Sidebar />
-      <div className='profileRight'>
-        <Topber />
-        <div className='profileMainWrapper'>
+      <div className="profileRight">
+        <Topber userData={userData} />
+        <div className="profileMainWrapper">
           <h2>User Profile</h2>
-          <div className='formWrapper'>
-            <div className='inputsWrapper'>
-              <div className='singleInput'>
-                <p>Fullname</p>
-                <div className='inputWrapper'>
+          <div className="formWrapper">
+            <div className="profileInputsWrapper">
+              <div className="profileSingleInput">
+                <p>Full Name</p>
+                <div className="profileInputWrapper">
                   <input
-                    type='text'
-                    className='input'
+                    type="text"
+                    className="profileInput"
                     disabled
                     value={fullName}
                   />
                 </div>
               </div>
 
-              <div className='singleInput'>
+              <div className="profileSingleInput">
                 <p>Email</p>
-                <div className='inputWrapper'>
+                <div className="profileInputWrapper">
                   <input
-                    type='email'
-                    className='input'
+                    type="email"
+                    className="profileInput"
                     value={email}
                     disabled
                   />
                 </div>
               </div>
-              <div className='singleInput'>
+              <div className="profileSingleInput">
                 <p>Phone Number</p>
-                <div className='inputWrapper'>
+                <div className="profileInputWrapper">
                   <input
-                    type='number'
-                    className='input'
+                    type="number"
+                    className="profileInput"
                     disabled
                     value={phoneNumber}
                   />
                 </div>
               </div>
-              <div className='singleInput'>
+              <div className="profileSingleInput">
                 <p>Role</p>
-                <div className='inputWrapper'>
-                  <input
-                    type='text'
-                    className='input'
+                <div className="profileInputWrapper">
+                  <select
+                    name="roleSelect"
+                    id="roleSelect"
                     disabled
-                    value={loggedInUserRole}
-                  />
+                    className="profileInput"
+                    defaultValue={loggedInUserRole}
+                  >
+                    <option value={loggedInUserRole}>{loggedInUserRole}</option>
+                    <option value="Admin">Admin</option>
+                    <option value="labScientist">Lab Scientist</option>
+                  </select>
                 </div>
               </div>
             </div>
-            {/* <button className='profileEditBtn'>Edit</button> */}
+            {/* <div className='editWrapper'>
+              <button className='profileEditBtn'>Edit</button>
+            </div> */}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
