ffefb288dc9fb3e350b36af651d677b1b02a5308
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-08-11 10:21:45 +0100

    made homepage responsive and added sidebar toggle functionality

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,15 +4,35 @@ import "./sidebar.scss";
 import LogoImg from "../../utils/images/sidebarBiopath2.png";
 import { RxDashboard } from "react-icons/rx";
 import { BsClipboardCheck, BsFillPersonFill } from "react-icons/bs";
-import { MdCategory, MdManageAccounts, MdSchedule } from "react-icons/md";
+import {
+  MdCancel,
+  MdCategory,
+  MdManageAccounts,
+  MdSchedule,
+} from "react-icons/md";
 import { RiTeamFill } from "react-icons/ri";
 import { FaFileInvoice } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
+import { IoIosMenu } from "react-icons/io";
 import { NavLink } from "react-router-dom";
 import AlertDialogSlide from "../Dialogue";
 
 const Sidebar = () => {
   const [open, setOpen] = React.useState(false);
+
+  // SIDEBAR STATUS
+  const [sidebarOpen, setSidebarOpen] = React.useState(false);
+
+  // FUNCTION TO TOGGLE SIDEBAR
+  const handleSidebarToggle = (action) => {
+    if (action === "showSidebar") {
+      setSidebarOpen(true);
+    }
+    if (action === "hideSidebar") {
+      setSidebarOpen(false);
+    }
+  };
+  // END OF FUNCTION TO TOGGLE SIDEBAR
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -27,130 +47,167 @@ const Sidebar = () => {
   // }
 
   return (
-    <div className="sidebarWrapper">
-      <AlertDialogSlide
-        open={open}
-        handleClose={handleClose}
-        message=" Are you sure you want to logout?"
-        link="/login"
-        title="Logout"
-      />
-      <div className="sidebarTop">
-        <div className="sidebarTopImageWrapper">
-          <img src={LogoImg} alt="Logo" /> <span>Biopath MedLab</span>
+    <>
+      {!sidebarOpen && (
+        <div
+          className="menuIconWrapper"
+          onClick={() => handleSidebarToggle("showSidebar")}
+        >
+          <IoIosMenu className="menuIcon" />
         </div>
-      </div>
-      <div className="sidebarBottom">
-        <div className="sidebarBottomTop">
-          <ul className="ulTitle">
-            MAIN
-            <NavLink to="/" style={{ textDecoration: "none" }}>
-              {({ isActive }) => (
-                <li
-                  className={
-                    isActive ? "activeLink sidebarListItem" : "sidebarListItem"
-                  }
-                >
-                  <RxDashboard className="sidebarIcon" />
-                  <span> Dashboard</span>
-                </li>
-              )}
-            </NavLink>
-          </ul>
-          <ul className="ulTitle">
-            LIST
-            <NavLink to="/scheduleCandidate" style={{ textDecoration: "none" }}>
-              {({ isActive }) => (
-                <li
-                  className={
-                    isActive ? "activeLink sidebarListItem" : "sidebarListItem"
-                  }
-                >
-                  <MdSchedule className="sidebarIcon" />
-                  <span> Schedule Candidate</span>
-                </li>
-              )}
-            </NavLink>
-            <NavLink to="/partnerLabs" style={{ textDecoration: "none" }}>
-              {({ isActive }) => (
-                <li
-                  className={
-                    isActive ? "activeLink sidebarListItem" : "sidebarListItem"
-                  }
-                >
-                  <BsClipboardCheck className="sidebarIcon" />
-                  <span> Partner Labs</span>
-                </li>
-              )}
-            </NavLink>
-            <NavLink to="/manageStaff" style={{ textDecoration: "none" }}>
-              {({ isActive }) => (
-                <li
-                  className={
-                    isActive ? "activeLink sidebarListItem" : "sidebarListItem"
-                  }
-                >
-                  <MdManageAccounts className="sidebarIcon" />
+      )}
+      <div className={`sidebarWrapper ${sidebarOpen ? "open" : ""}`}>
+        <AlertDialogSlide
+          open={open}
+          handleClose={handleClose}
+          message=" Are you sure you want to logout?"
+          link="/login"
+          title="Logout"
+        />
+        <div className="sidebarTop">
+          <div className="sidebarTopImageWrapper">
+            <img src={LogoImg} alt="Logo" /> <span>Biopath MedLab</span>
+          </div>
+          <div
+            className="cancelIconWrapper"
+            onClick={() => handleSidebarToggle("hideSidebar")}
+          >
+            <MdCancel className="cancelIcon" />
+          </div>
+        </div>
+        <div className="sidebarBottom">
+          <div className="sidebarBottomTop">
+            <ul className="ulTitle">
+              MAIN
+              <NavLink to="/" style={{ textDecoration: "none" }}>
+                {({ isActive }) => (
+                  <li
+                    className={
+                      isActive
+                        ? "activeLink sidebarListItem"
+                        : "sidebarListItem"
+                    }
+                  >
+                    <RxDashboard className="sidebarIcon" />
+                    <span> Dashboard</span>
+                  </li>
+                )}
+              </NavLink>
+            </ul>
+            <ul className="ulTitle">
+              LIST
+              <NavLink
+                to="/scheduleCandidate"
+                style={{ textDecoration: "none" }}
+              >
+                {({ isActive }) => (
+                  <li
+                    className={
+                      isActive
+                        ? "activeLink sidebarListItem"
+                        : "sidebarListItem"
+                    }
+                  >
+                    <MdSchedule className="sidebarIcon" />
+                    <span> Schedule Candidate</span>
+                  </li>
+                )}
+              </NavLink>
+              <NavLink to="/partnerLabs" style={{ textDecoration: "none" }}>
+                {({ isActive }) => (
+                  <li
+                    className={
+                      isActive
+                        ? "activeLink sidebarListItem"
+                        : "sidebarListItem"
+                    }
+                  >
+                    <BsClipboardCheck className="sidebarIcon" />
+                    <span> Partner Labs</span>
+                  </li>
+                )}
+              </NavLink>
+              <NavLink to="/manageStaff" style={{ textDecoration: "none" }}>
+                {({ isActive }) => (
+                  <li
+                    className={
+                      isActive
+                        ? "activeLink sidebarListItem"
+                        : "sidebarListItem"
+                    }
+                  >
+                    <MdManageAccounts className="sidebarIcon" />
 
-                  <span> Manage Staff</span>
-                </li>
-              )}
-            </NavLink>
-            <NavLink to="/manageCandidates" style={{ textDecoration: "none" }}>
-              {({ isActive }) => (
-                <li
-                  className={
-                    isActive ? "activeLink sidebarListItem" : "sidebarListItem"
-                  }
-                >
-                  <FaFileInvoice className="sidebarIcon" />
+                    <span> Manage Staff</span>
+                  </li>
+                )}
+              </NavLink>
+              <NavLink
+                to="/manageCandidates"
+                style={{ textDecoration: "none" }}
+              >
+                {({ isActive }) => (
+                  <li
+                    className={
+                      isActive
+                        ? "activeLink sidebarListItem"
+                        : "sidebarListItem"
+                    }
+                  >
+                    <FaFileInvoice className="sidebarIcon" />
 
-                  <span> Manage Candidate</span>
-                </li>
-              )}
-            </NavLink>
-            <NavLink to="/manageClients" style={{ textDecoration: "none" }}>
-              {({ isActive }) => (
-                <li
-                  className={
-                    isActive ? "activeLink sidebarListItem" : "sidebarListItem"
-                  }
-                >
-                  <RiTeamFill className="sidebarIcon" />
-                  <span> Manage Clients</span>
-                </li>
-              )}
-            </NavLink>
-          </ul>
+                    <span> Manage Candidate</span>
+                  </li>
+                )}
+              </NavLink>
+              <NavLink to="/manageClients" style={{ textDecoration: "none" }}>
+                {({ isActive }) => (
+                  <li
+                    className={
+                      isActive
+                        ? "activeLink sidebarListItem"
+                        : "sidebarListItem"
+                    }
+                  >
+                    <RiTeamFill className="sidebarIcon" />
+                    <span> Manage Clients</span>
+                  </li>
+                )}
+              </NavLink>
+            </ul>
 
-          <ul className="ulTitle">
-            TEST
-            <NavLink to="/manageTests" style={{ textDecoration: "none" }}>
-              {({ isActive }) => (
-                <li
-                  className={
-                    isActive ? "activeLink sidebarListItem" : "sidebarListItem"
-                  }
-                >
-                  <FaFileInvoice className="sidebarIcon" />
-                  <span> Manage Tests</span>
-                </li>
-              )}
-            </NavLink>
-            <NavLink to="/testCategories" style={{ textDecoration: "none" }}>
-              {({ isActive }) => (
-                <li
-                  className={
-                    isActive ? "activeLink sidebarListItem" : "sidebarListItem"
-                  }
-                >
-                  <MdCategory className="sidebarIcon" />
-                  <span> Test Categories</span>
-                </li>
-              )}
-            </NavLink>
-          </ul>
-          {/* <ul className='ulTitle'>
+            <ul className="ulTitle">
+              TEST
+              <NavLink to="/manageTests" style={{ textDecoration: "none" }}>
+                {({ isActive }) => (
+                  <li
+                    className={
+                      isActive
+                        ? "activeLink sidebarListItem"
+                        : "sidebarListItem"
+                    }
+                  >
+                    <FaFileInvoice className="sidebarIcon" />
+                    <span> Manage Tests</span>
+                  </li>
+                )}
+              </NavLink>
+              <NavLink to="/testCategories" style={{ textDecoration: "none" }}>
+                {({ isActive }) => (
+                  <li
+                    className={
+                      isActive
+                        ? "activeLink sidebarListItem"
+                        : "sidebarListItem"
+                    }
+                  >
+                    <MdCategory className="sidebarIcon" />
+                    <span> Test Categories</span>
+                  </li>
+                )}
+              </NavLink>
+            </ul>
+            {/* <ul className='ulTitle'>
             ACCOUNT
             <NavLink to='/generateInvoice' style={{ textDecoration: 'none' }}>
               {({ isActive }) => (
@@ -165,23 +222,25 @@ const Sidebar = () => {
               )}
             </NavLink>
           </ul> */}
-        </div>
-        <div className="sidebarBottomBottom">
-          <ul className="ulTitle">
-            USER
-            <NavLink to="/profile" style={{ textDecoration: "none" }}>
-              {({ isActive }) => (
-                <li
-                  className={
-                    isActive ? "activeLink sidebarListItem" : "sidebarListItem"
-                  }
-                >
-                  <BsFillPersonFill className="sidebarIcon" />
-                  <span> Profile</span>
-                </li>
-              )}
-            </NavLink>
-            {/* <NavLink
+          </div>
+          <div className="sidebarBottomBottom">
+            <ul className="ulTitle">
+              USER
+              <NavLink to="/profile" style={{ textDecoration: "none" }}>
+                {({ isActive }) => (
+                  <li
+                    className={
+                      isActive
+                        ? "activeLink sidebarListItem"
+                        : "sidebarListItem"
+                    }
+                  >
+                    <BsFillPersonFill className="sidebarIcon" />
+                    <span> Profile</span>
+                  </li>
+                )}
+              </NavLink>
+              {/* <NavLink
               to='/logout'
               style={{ textDecoration: 'none' }}
               onClick={handleClickOpen}
@@ -190,18 +249,19 @@ const Sidebar = () => {
                 
               )}
             </NavLink> */}
-            {/* <li className='sidebarListItem' onClick={logout}>
+              {/* <li className='sidebarListItem' onClick={logout}>
               <FiLogOut className='sidebarIcon' />
               <span> Logout</span>
             </li> */}
-            <li className="sidebarListItem" onClick={handleClickOpen}>
-              <FiLogOut className="sidebarIcon" />
-              <span> Logout</span>
-            </li>
-          </ul>
+              <li className="sidebarListItem" onClick={handleClickOpen}>
+                <FiLogOut className="sidebarIcon" />
+                <span> Logout</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import DashboardCard from '../../components/dashboardCard/DashboardCard'
-import Sidebar from '../../components/sidebar/Sidebar'
-import Topber from '../../components/topbar/Topber'
-import Box from '@mui/material/Box'
-import { DataGrid } from '@mui/x-data-grid'
-import './home.scss'
+import React, { useEffect, useState } from "react";
+import DashboardCard from "../../components/dashboardCard/DashboardCard";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Topber from "../../components/topbar/Topber";
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+import "./home.scss";
 
-import { useSelector } from 'react-redux'
-import { privateRequest, publicRequest } from '../../functions/requestMethods'
-import Loading from '../../components/loading/Loading'
-import Error from '../../components/error/Error'
+import { useSelector } from "react-redux";
+import { privateRequest, publicRequest } from "../../functions/requestMethods";
+import Loading from "../../components/loading/Loading";
+import Error from "../../components/error/Error";
 
 const Home = () => {
   // GET CURRENT LOGGED IN USER
-  const { currentUser } = useSelector((state) => state?.user)
-  const userName = currentUser?.data?.profile?.fullName
+  const { currentUser } = useSelector((state) => state?.user);
+  const userName = currentUser?.data?.profile?.fullName;
 
-  const [pageSize, setPageSize] = useState(5)
-  const { token } = useSelector((state) => state?.user?.currentUser?.data)
+  const [pageSize, setPageSize] = useState(5);
+  const { token } = useSelector((state) => state?.user?.currentUser?.data);
   const columns = [
     {
-      field: 'fullName',
-      headerName: 'Staff Name',
+      field: "fullName",
+      headerName: "Staff Name",
       width: 320,
       editable: true,
     },
-    { field: 'phoneNumber', headerName: 'Phone Number', width: 250 },
-    { field: 'email', headerName: 'Email', width: 300 },
+    { field: "phoneNumber", headerName: "Phone Number", width: 250 },
+    { field: "email", headerName: "Email", width: 300 },
     // {
     //   field: "role",
     //   headerName: "Role",
@@ -39,111 +39,108 @@ const Home = () => {
     //     );
     //   },
     // },
-  ]
+  ];
 
   // SET LOADING AND ERROR FUNCTIONALITY
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   // useEffect to update error and loading state
   useEffect(() => {
-    console.log(error, loading)
-  }, [error, loading])
+    console.log(error, loading);
+  }, [error, loading]);
   // end of useEffect to update error and loading state
 
   // END OF SET LOADING AND ERROR FUNCTIONALITY
 
   // FUNCTIONALITIES TO GET ALL STAFF
 
-  const [staff, setStaff] = useState([])
+  const [staff, setStaff] = useState([]);
 
   const fetchStaff = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
 
-      const res = await publicRequest.get('/Staff', {
+      const res = await publicRequest.get("/Staff", {
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
-      })
+      });
       // const res = await privateRequest.get('Staff')
-      console.log('seccessS')
-      setStaff(res?.data?.data?.result)
-      setLoading(false)
-      console.log(res?.data?.data?.result)
+      console.log("seccessS");
+      setStaff(res?.data?.data?.result);
+      setLoading(false);
+      console.log(res?.data?.data?.result);
     } catch (error) {
-      setLoading(false)
-      setError(true)
-      setErrorMessage(error)
+      setLoading(false);
+      setError(true);
+      setErrorMessage(error);
 
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   // useeffect to call the fetchStaff function
   useEffect(() => {
-    fetchStaff()
+    fetchStaff();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
   // end of useeffect to call the fetchSyaff function
   // END OF FUNCTIONALITIES TO GET ALL STAFF
 
   // MISCELLANEOUS USEEFFECTS
   // update errorMessage state
-  useEffect(() => {}, [errorMessage])
+  useEffect(() => {}, [errorMessage]);
   // end of update errorMessage state
   // END OF MISCELLANEOUS USEEFFECTS
 
   return (
     <>
-      <div className='homeWrapper'>
+      <div className="homeWrapper">
         <Sidebar />
-        <div className='homepageRight'>
+        <div className="homepageRight">
           <Topber />
-          <div className='homeMainWrapperContainer'>
-            <div className='homeMainWrapper'>
-              <div className='homeMainTop'>
-                <DashboardCard type='scheduleCandidate' />
-                <DashboardCard type='manageClients' />
-                {/* <DashboardCard type='manageStaff' /> */}
-                <DashboardCard type='profile' userName={userName} />
-              </div>
 
-              <div className='homeMainBottom'>
-                <h3>All Staff</h3>
-                <Box sx={{ height: 350, width: '100%' }}>
-                  {loading || error ? (
-                    loading ? (
-                      <Loading />
-                    ) : (
-                      <Error errorMessage={errorMessage && errorMessage} />
-                    )
+          <div className="homeMainWrapper">
+            <div className="homeMainTop">
+              <DashboardCard type="scheduleCandidate" />
+              <DashboardCard type="manageClients" />
+              {/* <DashboardCard type='manageStaff' /> */}
+              <DashboardCard type="profile" userName={userName} />
+            </div>
+
+            <div className="homeMainBottom">
+              <h3>All Staff</h3>
+              <Box sx={{ height: 350, width: "100%" }}>
+                {loading || error ? (
+                  loading ? (
+                    <Loading />
                   ) : (
-                    <DataGrid
-                      rows={staff}
-                      columns={columns}
-                      pageSize={pageSize}
-                      checkboxSelection
-                      disableSelectionOnClick
-                      experimentalFeatures={{ newEditingApi: true }}
-                      onPageSizeChange={(newPageSize) =>
-                        setPageSize(newPageSize)
-                      }
-                      rowsPerPageOptions={[5, 10, 20]}
-                      pagination
-                      getRowId={(row) => row.userId}
-                    />
-                  )}
-                </Box>
-              </div>
+                    <Error errorMessage={errorMessage && errorMessage} />
+                  )
+                ) : (
+                  <DataGrid
+                    rows={staff}
+                    columns={columns}
+                    pageSize={pageSize}
+                    checkboxSelection
+                    disableSelectionOnClick
+                    experimentalFeatures={{ newEditingApi: true }}
+                    onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                    rowsPerPageOptions={[5, 10, 20]}
+                    pagination
+                    getRowId={(row) => row.userId}
+                  />
+                )}
+              </Box>
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
